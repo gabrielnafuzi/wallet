ade270e8db7c1b8c680661ed5eb6364b72a8b4d3
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-16 04:05:13 -0300

    feat: add animation to forms

--- src/features/wallet/components/go-back-header/go-back-header.tsx
+++ src/features/wallet/components/go-back-header/go-back-header.tsx
@@ -1,6 +1,8 @@
 import { Button, Flex, Text } from '@chakra-ui/react'
 import { useLocation } from '@tanstack/react-location'
 
+import { AnimatedEntry } from '@/components/animated'
+
 type GoBackHeaderProps = {
   title: string
 }
@@ -13,21 +15,30 @@ export const GoBackHeader = ({ title }: GoBackHeaderProps) => {
   }
 
   return (
-    <Flex justify="space-between" align="center" mb="8">
-      <Text fontWeight="bold" fontSize="2xl">
-        {title}
-      </Text>
+    <AnimatedEntry
+      initial={{ opacity: 0, y: -40 }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        transition: { type: 'spring', bounce: 0.4 },
+      }}
+    >
+      <Flex justify="space-between" align="center" mb="8">
+        <Text fontWeight="bold" fontSize="2xl">
+          {title}
+        </Text>
 
-      <Button
-        onClick={handleGoBack}
-        bg="brand.gray.default"
-        size="sm"
-        w="6rem"
-        _hover={{ bg: 'brand.gray.darken' }}
-        _active={{ bg: 'brand.gray.darken' }}
-      >
-        Go back
-      </Button>
-    </Flex>
+        <Button
+          onClick={handleGoBack}
+          bg="brand.gray.default"
+          size="sm"
+          w="6rem"
+          _hover={{ bg: 'brand.gray.darken' }}
+          _active={{ bg: 'brand.gray.darken' }}
+        >
+          Go back
+        </Button>
+      </Flex>
+    </AnimatedEntry>
   )
 }
