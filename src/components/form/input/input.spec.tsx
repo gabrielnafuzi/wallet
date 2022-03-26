4b85270e1aec9338fc500c3c37555502589ad346
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-25 21:04:06 -0300

    test: ensure input render with label

---
+++ src/components/form/input/input.spec.tsx
@@ -0,0 +1,13 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from '@/utils/tests'
+
+import { Input } from '.'
+
+describe('<Input />', () => {
+  it('should render with label', () => {
+    renderWithTheme(<Input name="field" label="field" />)
+
+    expect(screen.queryByLabelText('field')).toBeInTheDocument()
+  })
+})
