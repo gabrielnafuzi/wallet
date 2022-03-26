466a340b3ea2c5fd927e4af2ae2156ab3d85c2df
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-25 23:02:58 -0300

    test: ensure WalletStore update a token correctly

--- src/store/wallet/wallet-store.spec.ts
+++ src/store/wallet/wallet-store.spec.ts
@@ -36,4 +36,19 @@ describe('WalletStore', () => {
 
     expect(result.current.tokens.length).toBe(0)
   })
+
+  it('should update a token', () => {
+    const { result } = renderHook(() => useWalletStore())
+
+    expect(result.current.tokens.length).toBe(0)
+
+    const token = makeToken()
+
+    act(() => {
+      result.current.addToken(token)
+      result.current.updateToken({ ...token, balance: '200' })
+    })
+
+    expect(result.current.tokens[0].balance).toBe('200')
+  })
 })
