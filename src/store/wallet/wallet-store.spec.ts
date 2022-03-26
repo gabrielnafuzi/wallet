d41a33327ef2f3f1cdcbb8f9a86a8da77e108be3
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-25 22:59:07 -0300

    test: ensure WalletStore add a token correctly

---
+++ src/store/wallet/wallet-store.spec.ts
@@ -0,0 +1,24 @@
+import { act, renderHook } from '@testing-library/react-hooks'
+
+import type { Token } from './types'
+import { useWalletStore } from './wallet-store'
+
+const makeToken = (): Token => ({
+  id: 'token_id',
+  name: 'token_name',
+  balance: '0',
+})
+
+describe('WalletStore', () => {
+  it('should add a token', () => {
+    const { result } = renderHook(() => useWalletStore())
+
+    expect(result.current.tokens.length).toBe(0)
+
+    act(() => {
+      result.current.addToken(makeToken())
+    })
+
+    expect(result.current.tokens.length).toBe(1)
+  })
+})
