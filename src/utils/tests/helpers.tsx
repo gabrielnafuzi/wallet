180248d666c2ac6f69fef6f0ed03a3c0163089a9
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-25 22:37:34 -0300

    refator: change from ThemeProvider to ChakraProvider

--- src/utils/tests/helpers.tsx
+++ src/utils/tests/helpers.tsx
@@ -1,8 +1,8 @@
-import { ThemeProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import { render, RenderResult } from '@testing-library/react'
 
 import { theme } from '@/styles/theme'
 
 export const renderWithTheme = (children: React.ReactNode): RenderResult => {
-  return render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
+  return render(<ChakraProvider theme={theme}>{children}</ChakraProvider>)
 }
