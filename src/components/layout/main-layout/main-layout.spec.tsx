1e4e7caf34b2b4c9f3a3e7e8bf5f8aee9729266a
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-26 00:16:00 -0300

    test: ensure MainLayout renders children correctly

---
+++ src/components/layout/main-layout/main-layout.spec.tsx
@@ -0,0 +1,17 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from '@/utils/tests'
+
+import { MainLayout } from './main-layout'
+
+describe('<MainLayout />', () => {
+  it('should render children correctly', () => {
+    renderWithTheme(
+      <MainLayout>
+        <span>Hello</span>
+      </MainLayout>
+    )
+
+    expect(screen.getByText('Hello')).toBeInTheDocument()
+  })
+})
