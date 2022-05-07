149e257d8aea9363c485951ad82e27be82647b80
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-05-07 01:09:37 -0300

    refactor: move parseJSON to a folder of parses

--- src/utils/parsers/parse-json/parse-json.ts
+++ src/utils/parsers/parse-json/parse-json.ts
@@ -1,5 +1,5 @@
 /**
- * @description A wrapper for "JSON.parse()"" to support "undefined" value
+ * @description A wrapper for "JSON.parse()" to support "undefined" value
  * @param {(string | null)} value - The value to parse - should be a string or null
  */
 export const parseJSON = <T>(value: string | null): T | undefined => {
