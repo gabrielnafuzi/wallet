b91791c92e4c751b86b69bf36ad5b6c137a3d488
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-05-10 19:55:07 -0300

    test: ensure that is canceling the removal correctly

--- cypress/e2e/remove-token.ts
+++ cypress/e2e/remove-token.ts
@@ -17,4 +17,22 @@ describe('Remove token', () => {
 
     cy.findAllByRole('button', { name: /edit token/i }).should('have.length', 2)
   })
+
+  it('should cancel remove token correctly and return to /', () => {
+    cy.addToken({ name: 'BTC', balance: '200' })
+    cy.addToken({ name: 'ETH', balance: '300' })
+    cy.addToken({ name: 'LTC', balance: '400' })
+
+    cy.findAllByRole('button', { name: /edit token/i })
+      .first()
+      .click()
+
+    cy.findByRole('button', { name: /remove/i }).click()
+    cy.focused().click()
+    cy.findByRole('button', { name: /go back/i }).click()
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
+    cy.findAllByRole('button', { name: /edit token/i }).should('have.length', 3)
+  })
 })
